2b880c50251812946cdee8fe7ac26df47e10e3b9
alex <alexanderrosen45@gmail.com>
2022-11-05 17:42:32 -0400

    choose food

--- tPay/payments/views.py
+++ tPay/payments/views.py
@@ -17,23 +17,26 @@ def accept_payment(request):
 @login_required(login_url="/signin")
 @require_POST
 def authenticate_face(request):
-    context = {'signed_in': request.user.is_authenticated}
+    context = {'signed_in': request.user.is_authenticated, 'food_item': request.POST["food_item"]}
     if request.method == 'POST':
-        username = request.POST["username"]
-        image_path = request.POST["src"]
-        image = NamedTemporaryFile()
-        image.write(urlopen(path).read())
-        image.flush()
-        image = File(image)
-        name = str(image.name).split('\\')[-1]
-        name += '.jpg'  # store image in jpeg format
-        image.name = name
-        if image is not None:
-            obj = Image.objects.create(username=username, image=image)  # create a object of Image type defined in model
-            obj.save()
-            context["path"] = obj.image.url
-            context["username"] = obj.username
-        else :
-            return redirect('/')
-        return redirect('home')
+        try:
+            username = request.POST["username"]
+            image_path = request.POST["src"]
+            image = NamedTemporaryFile()
+            image.write(urlopen(path).read())
+            image.flush()
+            image = File(image)
+            name = str(image.name).split('\\')[-1]
+            name += '.jpg'  # store image in jpeg format
+            image.name = name
+            if image is not None:
+                obj = Image.objects.create(username=username, image=image)  # create a object of Image type defined in model
+                obj.save()
+                context["path"] = obj.image.url
+                context["username"] = obj.username
+            else :
+                return redirect('/')
+            return redirect('home')
+        except:
+            pass
     return render(request, "payments/payments.html", context=context)
